8e3393c6ebe1c64568e0e94c08cba694757b5a7a
Douglas-Josue <jhonatancortez708@gmail.com>
2020-11-27 06:15:07 -0600

    Puesta en marcha en un hosting

--- app/src/main/java/com/daute/proyectofinaldaute/Setting_VAR.java
+++ app/src/main/java/com/daute/proyectofinaldaute/Setting_VAR.java
@@ -5,18 +5,18 @@ public class Setting_VAR {
     public static final String io = "http://192.168.100.5/";
 
     public static final String URL_PRUEBA = "json1.php";
-    public static final String URL_GUARDAR_CATEGORIAS = "http://192.168.100.5/service2020/guardar_categorias.php";
-    public static final String URL_CONSULTAR_CATEGORIAS = "http://192.168.100.5/service2020/searchAllCategoria.php";
-    public static final String URL_DELETE_CATEGORIA = "http://192.168.100.5/service2020/eliminarCategoria.php";
-    public static final String URL_UPDATE_CATEGORIA = "http://192.168.100.5/service2020/actualizar.php";
-    public static final String URL_LOGIN_USUARIO = "http://192.168.100.5/service2020/login.php";
-    public static final String URL_LISTA_USUARIO = "http://192.168.100.5/service2020/searchAllUsuarios.php";
-    public static final String URL_GUARDAR_USUARIO = "http://192.168.100.5/service2020/guardarUsuario.php";
-    public static final String URL_DELETE_USUARIO = "http://192.168.100.5/service2020/eliminarUsuario.php";
-    public static final String URL_UPDATE_USUARIO = "http://192.168.100.5/service2020/actualizarUsuario.php";
-    public static final String URL_LISTA_PRODUCTOS = "http://192.168.100.5/service2020/searchAllProductos.php";
-    public static final String URL_REGISTRAR_PRODUCTOS = "http://192.168.100.5/service2020/guardar_productos.php";
-    public static final String URL_SPINNER_CAT = "http://192.168.100.5/service2020/searchAllCategoria.php";
-    public static final String URL_DELETE_PRODUCTO = "http://192.168.100.5/service2020/eliminarProducto.php";
-    public static final String URL_UPDATE_PRODUCTO = "http://192.168.100.5/service2020/actualizarProducto.php";
+    public static final String URL_GUARDAR_CATEGORIAS = "https://appiandroid.000webhostapp.com/guardar_categorias.php";
+    public static final String URL_CONSULTAR_CATEGORIAS = "https://appiandroid.000webhostapp.com/searchAllCategoria.php";
+    public static final String URL_DELETE_CATEGORIA = "https://appiandroid.000webhostapp.com/eliminarCategoria.php";
+    public static final String URL_UPDATE_CATEGORIA = "https://appiandroid.000webhostapp.com/actualizar.php";
+    public static final String URL_LOGIN_USUARIO = "https://appiandroid.000webhostapp.com/login.php";
+    public static final String URL_LISTA_USUARIO = "https://appiandroid.000webhostapp.com/searchAllUsuarios.php";
+    public static final String URL_GUARDAR_USUARIO = "https://appiandroid.000webhostapp.com/guardarUsuario.php";
+    public static final String URL_DELETE_USUARIO = "https://appiandroid.000webhostapp.com/eliminarUsuario.php";
+    public static final String URL_UPDATE_USUARIO = "https://appiandroid.000webhostapp.com/actualizarUsuario.php";
+    public static final String URL_LISTA_PRODUCTOS = "https://appiandroid.000webhostapp.com/searchAllProductos.php";
+    public static final String URL_REGISTRAR_PRODUCTOS = "https://appiandroid.000webhostapp.com/guardar_productos.php";
+    public static final String URL_SPINNER_CAT = "https://appiandroid.000webhostapp.com/searchAllCategoria.php";
+    public static final String URL_DELETE_PRODUCTO = "https://appiandroid.000webhostapp.com/eliminarProducto.php";
+    public static final String URL_UPDATE_PRODUCTO = "https://appiandroid.000webhostapp.com/actualizarProducto.php";
 }
